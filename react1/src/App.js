863a7a92c756a113704079c781eae7722afa88ae
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-10-04 15:35:13 -0300

    modulo 2 - aula 6 - esttilizacao com StyledComponents (1/3)

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function Avatar(props) {
-  return (
-    <div style={{ backgroundColor: '#FF0000', padding: 20, width: 150 }}>
-      <img style={{ width:50, height: 50 }} src={props.user.url} alt={props.user.name} />
-      <br />
-      <span>{props.user.name}</span>
-    </div>
-  ); 
-}
+const Site = styled.div `
+  width: 400px;
+  height: 400px;
+  background-color: yellow;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1 `
+  color: #f00;
+  font-size: 18px;
+`;
+
+const Botao = styled.button `
+  font-size: 20px;
+  color: white;
+  background-color: black;
+  border: 0;
+  cursor: pointer;
+`;
 
 function App() {
 
-  const user = {
-    url: 'https://www.google.com/google.jpg',
-    name: 'Gustavo Alves'
-  };
-
-  return <>
-    <Avatar user={user} />
-  </>;
+  return (
+    <Site>
+      <Title>Título bem Legal</Title>
+      <Botao>Clique Aqui</Botao>
+    </Site>
+  );
 }
 
 export default App;

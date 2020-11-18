fe3e8e32945bd4d339c69c58d00d0c82bbe6ea05
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2020-11-17 23:30:33 -0300

    modulo 2 - aula 9 - useState

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,41 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Site = styled.div `
-  width: 400px;
-  height: 400px;
-  background-color: yellow;
-  margin: 0 auto;
-`;
-
-const Title = styled.h1 `
-  color: #f00;
-  font-size: 18px;
-`;
-
-const Botao = styled.button `
-  font-size: 19px;
-  padding: 15px;
-  border: 3px dashed #f00;
-  color: #f00;
-  background-color: #fff;
-  margin: 5px;
-  border-radius: 5px;
-`;
-
-const BotaoPequeno = styled(Botao) `
-  padding: 5px 10px;
-  font-size: 16px;
-`;
+import React, { useState } from 'react';
 
 function App() {
 
+  const [ contagem, setContagem ] = useState( 0 )
+
+  const botaoAction = () => {
+    setContagem( contagem + 1 );
+  };
+
   return (
-    <Site>
-      <Title>Título bem Legal</Title>
-      <Botao>Clique Aqui</Botao>
-      <BotaoPequeno>Clique Aqui</BotaoPequeno>
-    </Site>
+    <>
+      <div>{contagem} Vezes</div>
+      <button onClick={botaoAction}>Clique para aumentar</button>
+    </>
   );
 }
 

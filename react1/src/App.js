1b7c9c485b442d8274d03d1b57d94e26ea509951
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2021-01-27 18:14:30 -0300

    Conceitos Básicos  de React - 05 - Campo de Input

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  width:400px;
+  height:30px;
+  font-size:16px;
+  padding:10px;
+  border: 1px solid #000;
+`;
 
 function App() {
 
-  const [ contagem, setContagem ] = useState( 0 )
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
 
-  const botaoAction = () => {
-    setContagem( contagem + 1 );
+  const handleEmailInput = e => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordInput = e => {
+    setPassword(e.target.value);
+  };
+
+  const handleButton = () => {
+    alert(email + ' - ' + password);
   };
 
   return (
+
     <>
-      <div>{contagem} Vezes</div>
-      <button onClick={botaoAction}>Clique para aumentar</button>
+      <Input placeholder="Digite seu e-mail" type="email" value={email} onChange={handleEmailInput} />
+      <Input placeholder="Digite sua senha" type="password" value={password} onChange={handlePasswordInput} />
+      <button onClick={handleButton}>Dizer</button>
     </>
+
   );
+
 }
 
 export default App;
